0b4f56f80459c17219f8ae6044c62d8fad2ffe5d
ms <if23b212@technikum-wien.at>
2025-12-02 19:14:53 +0100

    Fix tests to match DTO & service structure

--- tests/test_database.py
+++ tests/test_database.py
@@ -1,49 +1,53 @@
 import unittest
-import importlib
 import sqlite3
 
 from database import database_sm
-from model.user import User
+from dto.RequesDTO import CreateUserDTO, CreatePostDTO
+from service.userService import createUser
+from service.postService import createPost
 
 
 class TestDatabase(unittest.TestCase):
     def setUp(self):
-        # Reload to reset tables for isolation between tests
-        self.db = importlib.reload(database_sm)
+        database_sm.cur.execute("DELETE FROM post")
+        database_sm.cur.execute("DELETE FROM user")
+        database_sm.con.commit()
+        self.db = database_sm
 
     def test_save_user_and_post(self):
-        user = User("Mine", "Tester")
-        self.db.save_user(user)
+        user_dto = CreateUserDTO(first_name="Mine", last_name="Tester")
+        user = createUser(user_dto)
+
         self.assertIsNotNone(user.id)
 
-        post = user.add_post("", "Test text 123")
-        self.db.save_post(post)
+        post_dto = CreatePostDTO(image="", text="Test text 123", user_id=user.id)
+        post = createPost(post_dto)
+
         self.assertIsNotNone(post.id)
 
         urow = self.db.cur.execute(
-            "SELECT first_name,last_name FROM user WHERE id=?",
+            "SELECT first_name, last_name FROM user WHERE id = ?",
             (user.id,),
         ).fetchone()
         self.assertEqual(urow, ("Mine", "Tester"))
 
         prow = self.db.cur.execute(
-            "SELECT image,text,user_id FROM post WHERE id=?",
+            "SELECT image, text, user_id FROM post WHERE id = ?",
             (post.id,),
         ).fetchone()
         self.assertEqual(prow, ("", "Test text 123", user.id))
 
     def test_unique_post_text_constraint(self):
-        user = User("Uniq", "Owner")
-        self.db.save_user(user)
+        user_dto = CreateUserDTO(first_name="Sana", last_name="Bhutto")
+        user = createUser(user_dto)
 
-        p1 = user.add_post("", "unique text")
-        self.db.save_post(p1)
+        p1_dto = CreatePostDTO(image="", text="unique text", user_id=user.id)
+        createPost(p1_dto)
 
-        p2 = user.add_post("", "unique text")
+        p2_dto = CreatePostDTO(image="", text="unique text", user_id=user.id)
         with self.assertRaises(sqlite3.IntegrityError):
-            self.db.save_post(p2)
+            createPost(p2_dto)
 
 
 if __name__ == "__main__":
     unittest.main()
-
